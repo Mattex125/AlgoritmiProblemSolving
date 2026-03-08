cf7bd4dd5bed78a8508adc20c98e691a4ad09baf
Mattex125 <matteomerollim1@gmail.com>
2026-03-08 12:22:09 +0100

    extra_Prob

---
+++ x1ExactlyElectrical.c
@@ -0,0 +1,24 @@
+//https://dmoj.ca/problem/ccc17j3
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    int x, y;
+} Point;
+
+bool can_connect_exactly(Point a, Point b, int n) {
+    int d = abs(a.x - b.x) + abs(a.y - b.y);  // axis-only distance
+    return (n >= d) && ((n - d) % 2 == 0); //return true
+    //if it's reachable and steps to reach is pair all good (wasting takes 2 steps)
+}
+
+int main(void) {
+    Point start, end;
+    int n;
+    // input: x1 y1 x2 y2 n
+    scanf("%d %d %d %d %d", &start.x, &start.y, &end.x, &end.y, &n);
+    printf("%s\n", can_connect_exactly(start, end, n) ? "Y" : "N");
+    return 0;
+}
