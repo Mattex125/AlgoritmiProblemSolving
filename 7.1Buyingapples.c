f1bdcfef0c4333c5072a4b1244cb805e1a58d476
Mattex125 <matteomerollim1@gmail.com>
2026-03-29 15:53:22 +0200

    buying apples with ia and not using memo technique

---
+++ 7.1Buyingapples.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct promo { //promos
+    double prize;
+    int nobjs;
+    struct promo* next;
+} promo;
+
+typedef struct node { //lista di k to test
+    int val;
+    struct node* next;
+} node;
+
+int nelems = 0;
+
+/* ---- memo ---- */
+double *memo = NULL;
+int MEMSIZE = 0;
+
+promo* addPromo(promo* head, double prize, int nobjs) {
+    promo* newnode = (promo*)malloc(sizeof(promo));
+    newnode->prize = prize;
+    newnode->nobjs = nobjs;
+    newnode->next = head;
+    nelems++;
+    return newnode;
+}
+
+node* enqueue(node* head, int val) {
+    node* newnode = (node*)malloc(sizeof(node));
+    newnode->val = val;
+    newnode->next = NULL;
+
+    if (head == NULL)
+        return newnode;
+
+    node* tmp = head;
+    while (tmp->next != NULL)
+        tmp = tmp->next;
+
+    tmp->next = newnode;
+    return head;
+}
+
+int maxNobjs(promo* head) {
+    int max = 0;
+    while (head != NULL) {
+        if (head->nobjs > max)
+            max = head->nobjs;
+        head = head->next;
+    }
+    return max;
+}
+
+double solvek(promo* head, int nelems, double unit_price) {
+    if (nelems <= 0)
+        return 0;
+
+    if (nelems < MEMSIZE && memo[nelems] >= 0)
+        return memo[nelems];
+
+    double min = unit_price * nelems; // compra tutto senza promo
+
+    promo* curr = head;
+    while (curr != NULL) {
+        double val = curr->prize + solvek(head, nelems - curr->nobjs, unit_price);
+        if (val < min)
+            min = val;
+        curr = curr->next;
+    }
+
+    if (nelems < MEMSIZE)
+        memo[nelems] = min;
+
+    return min;
+}
+
+void freePromoList(promo* head) {
+    promo* tmp;
+    while (head != NULL) {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+    nelems = 0;
+}
+
+void freeNodeList(node* head) {
+    node* tmp;
+    while (head != NULL) {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+}
+
+/* ---- main ---- */
+
+int main() {
+    double unit_price;
+    int npromo;
+    int case_num = 1;
+
+    while (scanf("%lf %d", &unit_price, &npromo) == 2) { //read case
+
+        promo* plist = NULL;
+        node* list = NULL;
+
+        for (int i = 0; i < npromo; i++) { //read all promos
+            int nobj;
+            double p;
+            scanf("%d %lf", &nobj, &p);
+            plist = addPromo(plist, p, nobj);
+        }
+
+        int x;
+        while (scanf("%d", &x) == 1) {
+            list = enqueue(list, x);
+            int c = getchar();
+            if (c == '\n' || c == EOF)
+                break;
+        }
+
+        printf("Case %d:\n", case_num++);
+
+        node* tmp = list;
+        int maxobj = maxNobjs(plist);
+
+        /* ---- alloc memo ---- */
+        int maxk = 0;
+        node* t2 = list;
+        while (t2 != NULL) {
+            if (t2->val > maxk)
+                maxk = t2->val;
+            t2 = t2->next;
+        }
+
+        MEMSIZE = maxk + maxobj + 1;
+        memo = (double*)malloc(sizeof(double) * MEMSIZE);
+        for (int i = 0; i < MEMSIZE; i++)
+            memo[i] = -1;
+        memo[0] = 0;
+
+        while (tmp != NULL) {
+            double min = 1e9;
+
+            for (int k = tmp->val; k <= tmp->val + maxobj; k++) {
+                double res = solvek(plist, k, unit_price);
+                if (res < min)
+                    min = res;
+            }
+
+            printf("Buy %d for $%.2f\n", tmp->val, min);
+
+            tmp = tmp->next;
+        }
+
+        free(memo);
+        freePromoList(plist);
+        freeNodeList(list);
+    }
+
+    return 0;
+}
---
+++ 7Buyingapples.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIN2(a,b) ((a) < (b) ? (a) : (b))
+
+typedef struct NodeOfBuys
+{
+    struct NodeOfBuys *next;
+    int n;
+}NodeOfBuys;
+
+void insertTail(NodeOfBuys **head, int value) {
+    NodeOfBuys *newNode = malloc(sizeof(NodeOfBuys));
+    newNode->n = value;
+    newNode->next = NULL;
+    
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    
+    NodeOfBuys *curr = *head;
+    while (curr->next != NULL) {
+        curr = curr->next;
+    }
+    curr->next = newNode;
+}
+
+int max_list(NodeOfBuys *head) { //per trovare il numero max k da gestire
+    int mx = 0;
+    while (head != NULL) {
+        if (head->n > mx) mx = head->n;
+        head = head->next;
+    }
+    return mx;
+}
+//k and only k appples
+double solve_k(int num[], double prize[], int nschemas, double unitprize, int n2buy,double memo[]) //num appe per prize, sono le promo
+{
+    double best, result;
+    if (memo[n2buy]!=-1)
+        return memo[n2buy];
+    if(n2buy==0) //actuaòòy not needed
+        return 0;
+    else{
+        result=solve_k(num,prize,nschemas,unitprize,n2buy-1,memo); //IP compro tutte piu una mela
+        best=result+unitprize; //best og n-1 mele +1 unitaria
+        for(int j=0; j<nschemas; j++){//controllo tutte le offerte
+            if(n2buy-num[j]>=0) //che possa doverle comprare, CERCO SOLO CON ESATTAMENTE K mele 2buy
+                result=solve_k(num,prize,nschemas,unitprize,n2buy-num[j],memo);//tutte meno quelle che assumo di mangiare
+             //all of these per every prize scheme
+            best=MIN2(best,result+prize[j]); //the lowest with that i had and best +prize this choose       
+        }
+        return memo[n2buy]=best;
+    }
+}
+
+int max_array(const int *arr, int n) {
+    if (n <= 0) return 0; // or handle as error
+    int mx = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > mx) mx = arr[i];
+    }
+    return mx;
+}
+
+double solve(int num[], double prize[], int nschemas, double unitprize, int n2buy,double *memo){ //at least n apples
+    //prezzo minimo con k, k+1 until all offers
+    double best;
+    int i;
+    //try with k
+    best=solve_k(num,prize,nschemas,unitprize,n2buy,memo);
+    for (i=n2buy+1;i<=n2buy+max_array(num,nschemas);i++){ //when i stop:io ho pensato a n2buy+max in num: quelle che compro+la promo max
+        best=MIN2(best,solve_k(num,prize,nschemas,unitprize,i,memo) );
+    }
+    return best;
+}
+
+
+int main(){
+    double UnitPrize;
+    int Nschemas;
+    int Ncases=1;
+    while(scanf("%lf %d",&UnitPrize,&Nschemas)!= EOF)
+    {
+        int NumApples[Nschemas];
+        double PromoPrize[Nschemas];
+        for (int i=0;i<Nschemas;i++)
+            scanf("%d %lf",&NumApples[i],&PromoPrize[i]);
+        NodeOfBuys* head=NULL;
+        char terminator=0;
+        int nBuyTest;
+        while(terminator!='\n'){
+            scanf("%d%c",&nBuyTest,&terminator);
+            insertTail(&head,nBuyTest);
+        }
+        printf("Case %d:\n",Ncases++); 
+        int maxKtoTest=(max_array(NumApples, Nschemas)+max_list(head)+1);
+        double* memo = malloc( maxKtoTest * sizeof(double));//test piu grande +1 pk c'e 0
+        for (int i = 1; i < maxKtoTest; i++)
+            memo[i] = -1;        
+        memo[0]=0.00;
+        while(head!=NULL){
+            double result=solve(NumApples,PromoPrize,Nschemas,UnitPrize,head->n,memo);
+            printf("Buy %d for $%.2lf\n",head->n,result);
+            NodeOfBuys *placeholderDeallocator=head;
+            head=head->next;
+            free(placeholderDeallocator);
+            }
+        free(memo); //i free memo on every test to allocate a new one
+    }   
+    return 0;
+}
